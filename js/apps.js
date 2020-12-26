8d9748e17b6a21b126aa1e2506025fcff92f40a4
Tototto09889 <74698145+Tototto09889@users.noreply.github.com>
2020-12-26 17:00:23 +0700

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -7,8 +7,10 @@ function init() {
 <header >
    <div id="nav">
    </div>
-</header>
-<body>
+</header><div>
+<div id="content">
+</div>
+<br>
 <form><div class="content"><center><h2 style="color:red">Attention Please!</h2></center>
 <ul>
 <li>This Is The Index Page For Google Team Drives.</li>
@@ -18,11 +20,6 @@ function init() {
 </ul>
 </div>
 </form>
-</body>
-<div>
-<div id="content">
-</div>
-<br>
 <footer class="text-muted"> <div class="container"> <p class="float-right"> <a href="#">Back to top</a> </p> ${UI.credit ? '<p>>Redesigned with <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="red" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" /> </svg> by <a href="https://bit.ly/3nGca0t" target="_blank">Rahman Ackerman</a>, based on Open Source Softwares.</p>' : ''} <p>Copyright © ${UI.copyright_year} <a href=" ${UI.company_link}" target="_blank"> ${UI.company_name}</a>, All Rights Reserved.</p> </div> </footer>
   `;
 	$('body').html(html);
